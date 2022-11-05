d3bb9884acf3629b38de9eacdfbc5098950e71f6
svegam <svegam23@gmail.com>
2022-11-05 17:07:33 -0500

    Clase 3 terminada

--- script.js
+++ script.js
@@ -25,7 +25,24 @@ const createTask= (evento) =>{
     //lo que permite este evento es que no recargue la página o actualice
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]")
-    console.log(input.value)
+    const value = input.value;
+    const list = document.querySelector("[data-list]")
+    const task = document.createElement("li");
+    //permite agregar la clase a la etiqueta <li> para que sirva el CSS
+    task.classList.add('card');
+    //permite poner el Input en blanco después de dar click en el botón
+    input.value = "";
+    
+    //backticks = ``
+    const content = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    task.innerHTML = content;
+    //Se agrega el hijo task(<li>) al padre list(<ul>)
+    list.appendChild(task);
+    console.log(content)
 }
 
 console.log(btn)
